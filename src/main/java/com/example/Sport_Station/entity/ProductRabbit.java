8e5dcc23e42d4559e67ea6baca46b0bfec49bc4e
Mikhael-Wellman <mikhaelwellman423@gmail.com>
2025-09-04 14:13:05 +0700

    add

--- src/main/java/com/example/Sport_Station/entity/ProductRabbit.java
+++ src/main/java/com/example/Sport_Station/entity/ProductRabbit.java
@@ -1,4 +1,20 @@
 package com.example.Sport_Station.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product_rabbit")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
 public class ProductRabbit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer stock;
 }
--- src/main/java/com/example/Sport_Station/service/impl/PdfService.java
+++ src/main/java/com/example/Sport_Station/service/impl/PdfService.java
@@ -1,4 +1,16 @@
 package com.example.Sport_Station.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PdfService {
+
+		public Resource getPdf(){
+
+			return new ClassPathResource("Mikhael Wellman.pdf");
+		}
 }
